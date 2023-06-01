5a98dd2b1064097c99d1bd3ab025eef571a98ed4
garfias06 <garfias06_23@outlook.com>
2023-06-01 16:49:04 -0500

    install js setup

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -2,39 +2,25 @@ const butInstall = document.getElementById('buttonInstall');
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
+window.addEventListener('beforeinstallprompt', (event) => {
+    window.deferredPrompt = event;
+    butInstall.classList.toggle('hidden', false);
+});
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    const promptEvent = window.deferredPrompt;
+
+    if (!promptEvent) {
+     return;
+    }
+    
+    promptEvent.prompt();
+    window.deferredPrompt = null;
+    butInstall.classList.toggle('hidden', true);
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
-
-
-
-
-
-// "devDependencies": {
-//   "@babel/core": "^7.21.8",
-//   "@babel/plugin-transform-runtime": "^7.22.2",
-//   "@babel/preset-env": "^7.22.2",
-//   "@babel/runtime": "^7.22.3",
-//   "@rollup/plugin-terser": "^0.4.3",
-//   "babel-loader": "^9.1.2",
-//   "css-loader": "^6.7.3",
-//   "html-webpack-plugin": "^5.5.0",
-//   "http-server": "^14.1.1",
-//   "style-loader": "^3.3.1",
-//   "webpack": "^5.83.1",
-//   "webpack-cli": "^5.1.1",
-//   "webpack-dev-server": "^4.15.0",
-//   "webpack-pwa-manifest": "^4.3.0",
-//   "workbox-webpack-plugin": "^6.5.4"
-// },
-// "dependencies": {
-//   "@jridgewell/sourcemap-codec": "^1.4.15",
-//   "code-mirror-themes": "^1.0.0",
-//   "idb": "^7.1.1",
-//   "workbox-background-sync": "^7.0.0"
-// }
-// }
+window.addEventListener('appinstalled', (event) => {
+    window.deferredPrompt = null;
+});
